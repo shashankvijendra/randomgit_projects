4c22b67755ebbbe541a789709e31b6161fbc460a
Shashank V <shashankvijendra@gmail.com>
2024-08-02 16:23:23 +0530

    function update

--- Bots/instagram_bot_python/video_download.py
+++ Bots/instagram_bot_python/video_download.py
@@ -1,11 +1,20 @@
 from instaloader import Instaloader, Profile
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from instagram import load_browser, Instagram
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-L = Instaloader(download_pictures=False)
-PROFILE = "sakshimalikk"
-profile = Profile.from_username(L.context, PROFILE)
+def download_video(username):
+    L = Instaloader(download_pictures=False)
+    PROFILE = username
+    profile = Profile.from_username(L.context, PROFILE)
 
-posts_sorted_by_likes = sorted(profile.get_posts(), key=lambda post: post.likes,reverse=True)
+    posts_sorted_by_likes = sorted(profile.get_posts(), key=lambda post: post.likes,reverse=True)
 
-for post in posts_sorted_by_likes:
-    if post.is_video:
-        L.download_post(post, PROFILE)
+    for post in posts_sorted_by_likes:
+        if post.is_video:
+            L.download_post(post, PROFILE)
+        
